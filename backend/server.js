71c1db8f5ee2822af06b92c43c317d4f6e6746b4
Vaishali Sahni <vaishalisahni10@gmail.com>
2026-04-20 01:43:19 +0530

    added frontend url in cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', process.env.CLIENT_URL],
   credentials: true,
 }));
 app.use(express.json());
